3d53ef2e0f791642f348da8ee26bcac6d4be6374
ihawn <ihawn.pl15@gmail.com>
2021-05-15 18:39:09 -0500

    -

--- MapGenerator.cs
+++ MapGenerator.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int mapWidth, mapHeight, octaves, seed;
+    public float noiseScale, lacunarity;
+
+    [Range(0, 1)]
+    public float persistance;
+
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+
+    public void GenerateMap()
     {
-        
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapHeight; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions [i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+        if(octaves < 0)
+        {
+            octaves = 0;
+        }
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
 }
